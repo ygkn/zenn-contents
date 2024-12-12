631f083b849a0d2317ac535c645496af3cf7b425
ygkn <ygkn@users.noreply.github.com>
2024-12-12 19:16:20 +0900

    wip

--- .textlintrc.js
+++ .textlintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     "preset-ja-technical-writing": true,
     "preset-ja-spacing": true,
+    "preset-jtf-style": true,
     "@proofdict/proofdict": {
       dictURL: "https://azu.github.io/proof-dictionary/",
     },
